f3e48fce277c8dde9c3d1878d4d4fae83ab03695
sajeewa malith <sajeewamalith0219@gmail.com>
2022-04-23 22:35:17 +0530

    Add TextViews for price

--- app/src/main/java/com/example/basics2/MainActivity.java
+++ app/src/main/java/com/example/basics2/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1500);
+        display(2);
+        displayPrice(2*5);
     }
 
     /**
@@ -26,5 +29,12 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
